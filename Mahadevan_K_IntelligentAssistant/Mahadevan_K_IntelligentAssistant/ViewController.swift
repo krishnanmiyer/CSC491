13721b460d042dfe1bb40add7670f77e3c7049c4
Krishnan Mahadevan <krishnan@Krishnan-Mahadevans-MacBook-Pro.local>
2017-05-29 12:11:08 -0700

    Initial Check In

---
+++ Mahadevan_K_IntelligentAssistant/Mahadevan_K_IntelligentAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mahadevan_K_IntelligentAssistant
+//
+//  Created by Krishnan Mahadevan on 5/29/17.
+//  Copyright © 2017 Krishnan Mahadevan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
